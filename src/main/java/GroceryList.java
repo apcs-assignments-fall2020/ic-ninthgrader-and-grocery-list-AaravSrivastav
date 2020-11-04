293dc8c15122cacdcdbff738bb36b1a6dc408609
AaravSrivastav <22asrivastav@athenian.org>
2020-11-04 00:39:59 +0000

    did the grocery list class

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,45 @@
 public class GroceryList {
-    
+    private String[] arr;
+
+    public GroceryList()
+    {
+        this.arr = new String[10];
+    }
+
+    public void add(String item)
+    {
+        for(int i = 0; i<arr.length; i++)
+        {
+            if(arr[i] == null)
+            {
+                arr[i] = item;
+            }
+            break;
+        }
+    }
+
+    public void remove(String item)
+    {
+        for(int i = 0; i<arr.length; i++)
+        {
+            if (arr[i] == item)
+            {
+                for(int j = i; j<arr.length-1; j++)
+                {
+                    arr[j] = arr[j+1];
+                }
+            }
+        }
+        arr[arr.length-1] = null;
+    }
+
+    public String toString()
+    {
+        String str = "";
+        for(int i = 0; i<arr.length; i++)
+        {
+            str += arr[i] + ", ";
+        }
+        return str;
+    }
 }
